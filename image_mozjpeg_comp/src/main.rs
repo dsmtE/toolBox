d5c0f3bcf76cda4696c644710b1e681fc49ea224
dsmtE <desmet.enguerrand@gmail.com>
2022-12-09 01:15:46 +0100

    |image_mozjpeg_comp] use glob and target multiples files
    - move some parts in functions
    - use glob to target multiple files and export them in a target folder path

--- image_mozjpeg_comp/src/main.rs
+++ image_mozjpeg_comp/src/main.rs
@@ -2,19 +2,20 @@ use clap::Parser;
 use image::{GenericImageView, imageops};
 use std::path::PathBuf;
 use std::io::Write;
+use glob::glob;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Arguments {
     #[arg(help("image input path"))]
-    src_image_path: PathBuf,
+    source_glob_pattern: String,
     dst_image_path: PathBuf,
 
     #[arg(short, long, default_value_t = 1200)]
     max_width: u32
 }
 
-fn mozjpeg_save(width: u32, height: u32, image_slice: &[u8], path: impl AsRef<std::path::Path>) -> Result<(), std::io::Error>{
+fn mozjpeg_save(width: u32, height: u32, image_slice: &[u8], path: & impl AsRef<std::path::Path>) -> Result<(), std::io::Error>{
 
     let mut comp = mozjpeg::Compress::new(mozjpeg::ColorSpace::JCS_RGB);
     
@@ -27,27 +28,44 @@ fn mozjpeg_save(width: u32, height: u32, image_slice: &[u8], path: impl AsRef<st
     comp.finish_compress();
     let jpeg_bytes_slice = comp.data_as_mut_slice().expect("Unable to xrite buffer to moz compressor");
 
-    let file = std::fs::File::create(path).unwrap_or_else(|e| panic!("{}", e));
+    let file = std::fs::File::create(path)?;
 
     std::io::BufWriter::new(file).write_all(jpeg_bytes_slice)
 }
+
+fn resize(img: image::DynamicImage, max_width: u32, filter: Option<imageops::FilterType>) -> image::DynamicImage {
+
+    let (width, height) = img.dimensions();
+    let target_width = std::cmp::min(max_width, width);
+    let target_height = (target_width as f32/width as f32 * height as f32) as u32;
+    
+    img.resize(target_width, target_height, filter.unwrap_or(imageops::Lanczos3))
+}
+
 fn main() {
     let args = Arguments::parse();
-    
     println!("{:?}", args);
 
-    let mut img = image::open(args.src_image_path.as_path()).expect("Unale to open the given image");
-    let (width, height) = img.dimensions();
-    
-    let target_width = std::cmp::min(args.max_width, width);
-    let target_height = (target_width as f32/width as f32 * height as f32) as u32;
+    for entry in glob(&args.source_glob_pattern).expect("Failed to read glob pattern") {
+        match entry {
+            Err(e) => println!("{:?}", e),
+            Ok(path) => {
+                println!("path found : {:?}", path.display());
+                let target_path = args.dst_image_path.join(path.file_name().unwrap());
 
-    println!("Resizing");
-    img = img.resize(target_width, target_height, imageops::Lanczos3);
-    
-    println!("Saving");
-    img.save(args.dst_image_path.as_path()).expect("Unable to save image at destination path");
-    mozjpeg_save(target_width, target_height, img.as_flat_samples_u8().unwrap().as_slice(), args.dst_image_path)
-        .expect("Unable to save the file");
-    
+                let mut img = image::open(&path)
+                    .expect(format!("Unale to open the given image at path {}", path.display()).as_str());
+                
+                print!("Resizing");
+                img = resize(img, args.max_width, None);
+                println!(", output size : {}x{}", img.width(), img.height());
+                
+                println!("Saving at {}", target_path.clone().display());
+                // img.save(args.dst_image_path.as_path()).expect("Unable to save image at destination path");
+                mozjpeg_save(img.width(), img.height(), img.as_flat_samples_u8().unwrap().as_slice(), &target_path)
+                    .expect("Unable to save the file");
+            },
+        }
+    }
+
 }
